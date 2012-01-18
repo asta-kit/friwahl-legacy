a9c9aa835de70a323abe8b2ef8b2ed9f302e4c3e
Mario Prausa <mariop@usta.de>
2012-01-18 17:38:42 +0100

    abfrage der wahlbeteiligung, bevor die erste nachricht gesendet wird

--- Client/wahlbet/ircbot.pl
+++ Client/wahlbet/ircbot.pl
@@ -16,10 +16,12 @@ my $conn = $irc->newconn(
 
 $conn->{channel} = '#uwahl';
 
+sub get_wahlbet {
+	system('ssh -i ~/.ssh/wahlbet_key wahlprognose@asta-wahl.asta.uni-karlsruhe.de wahl/Client/wahlbet/wahlbet.py wahl/Client/wahlbet/templates/wahlbet.irc.txt > /tmp/wahlbet.irc.txt');
+}
+
 sub send_wahlbet {
 	my ($conn, $nick) = @_;
-
-	system('ssh -i ~/.ssh/wahlbet_key wahlprognose@asta-wahl.asta.uni-karlsruhe.de wahl/Client/wahlbet/wahlbet.py wahl/Client/wahlbet/templates/wahlbet.irc.txt > /tmp/wahlbet.irc.txt');
 
 	open (FILE,"</tmp/wahlbet.irc.txt");
 
@@ -42,6 +44,8 @@ sub on_msg {
 	my ($conn, $event) = @_;
 
 	print "request from $event->{nick}\n";
+
+	get_wahlbet();
 
 	$conn->privmsg($event->{nick}, "Hallo $event->{nick},");
 	$conn->privmsg($event->{nick}, " ");
