fe74ad0d1234df5e2b3bc40394c7ad0fbf509151
Andreas Wolf <andreas.wolf@usta.de>
2015-07-02 20:53:35 +0200

    [BUGFIX] mkarchiso uses different root folder

--- FriWahlCD/preparecd.sh
+++ FriWahlCD/preparecd.sh
@@ -3,6 +3,8 @@
 RED="\033[40;1;31m"
 BLACK="\033[0m"
 
+ROOTFS=work/airootfs
+
 rm -rf work
 
 sed "s|%ustarepo%|$(readlink -f usta/repo)|g" usta/data/pacman.conf > /tmp/pacman.conf
@@ -19,10 +21,10 @@ mkarchiso -v -C /tmp/pacman.conf -p "openssh unzip zip irssi ipw2100-fw ipw2200-
 
 echo -e $RED"Kopiere System Dateien..."$BLACK
 
-rm -f work/root-image/etc/systemd/system/getty.target.wants/getty\@tty1.service
+rm -f $ROOTFS/etc/systemd/system/getty.target.wants/getty\@tty1.service
 
-cp -a usta/sysfiles/* work/root-image
-chown -R 1000:100 work/root-image/home/irc
+cp -a usta/sysfiles/* $ROOTFS
+chown -R 1000:100 $ROOTFS/home/irc
 
 echo -e $RED"Generiere Locales..."$BLACK
 mkarchiso -r "locale-gen" run
@@ -30,11 +32,11 @@ mkarchiso -r "locale-gen" run
 mkdir -p work/iso/arch/boot/i686
 
 echo -e $RED"Installiere ArchISO Hooks..."$BLACK
-make -C archiso DESTDIR=$(readlink -f work/root-image) install-initcpio
+make -C archiso DESTDIR=$(readlink -f $ROOTFS) install-initcpio
 
 echo -e $RED"Erstelle Wahl-Splash..."$BLACK
-cp work/root-image/etc/splash/usta/images/background.png /tmp
-usta/scripts/splash.sh people.dat /tmp/background.png work/root-image/etc/splash/usta/images/background.png
+cp $ROOTFS/etc/splash/usta/images/background.png /tmp
+usta/scripts/splash.sh people.dat /tmp/background.png $ROOTFS/etc/splash/usta/images/background.png
 rm /tmp/background.png
 
 echo -e $RED"Erstelle InitCPIO..."$BLACK
@@ -42,17 +44,17 @@ mkarchiso -r "mkinitcpio -k /boot/vmlinuz-linux -g /boot/archiso.img" run
 
 echo -e $RED"Installiere Kernel, InitCPIO und Bootloader..."$BLACK
 
-mv work/root-image/boot/archiso.img work/iso/arch/boot/i686
-mv work/root-image/boot/vmlinuz-linux work/iso/arch/boot/i686/vmlinuz
+mv $ROOTFS/boot/archiso.img work/iso/arch/boot/i686
+mv $ROOTFS/boot/vmlinuz-linux work/iso/arch/boot/i686/vmlinuz
 
 mkdir work/iso/arch/boot/syslinux
 
-cp work/root-image/usr/lib/syslinux/menu.c32 work/iso/arch/boot/syslinux
+cp $ROOTFS/usr/lib/syslinux/bios/menu.c32 work/iso/arch/boot/syslinux
 
 mkdir work/iso/isolinux
 
-cp work/root-image/usr/lib/syslinux/isolinux.bin work/iso/isolinux
-cp work/root-image/usr/lib/syslinux/isohdpfx.bin work/iso/isolinux
+cp $ROOTFS/usr/lib/syslinux/isolinux.bin work/iso/isolinux
+cp $ROOTFS/usr/lib/syslinux/isohdpfx.bin work/iso/isolinux
 cp usta/data/isolinux.cfg work/iso/isolinux/isolinux.cfg
 
 cp usta/data/aitab work/iso/arch
